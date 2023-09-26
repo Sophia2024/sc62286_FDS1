4b4b207e9e09a41f51b8deafc1821484850ebd5a
Sophia2024 <sophiachen@utexas.edu>
2023-09-26 13:54:53 -0500

    modules

---
+++ unit_conversions.py
@@ -0,0 +1,17 @@
+def miles_to_km(x) :
+    return x * 1.60934
+
+def km_to_miles(x) :
+    return x / 1.60934
+
+def f_to_c(x) :
+    return (x - 32) * 5 / 9
+    
+def c_to_f(x) :
+    return x * 9 / 5 + 32
+
+def kg_to_lb(x) :
+    return x * 2.2046 
+
+def lb_to_kg(x) :
+    return x / 2.2046 
